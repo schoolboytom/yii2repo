3732fecfce7da6f3563793c83d2564188150b440
Thomas Rutherford <tom.rutherford@hotmail.co.uk>
2015-07-02 21:57:33 +0100

    second upate commit

--- yii2/basic/views/site/index.php
+++ yii2/basic/views/site/index.php
@@ -26,7 +26,7 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Balls boobs</h2>
+                <h2>MY first go</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
